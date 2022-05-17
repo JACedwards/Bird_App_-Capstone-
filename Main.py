eb83f19419ced269d720bb0d4a2f4108c317b482
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-05-17 19:31:06 -0400

    After Chris session (backyard_bird) object prob

--- Main.py
+++ Main.py
@@ -35,8 +35,8 @@ from asyncio.constants import SENDFILE_FALLBACK_READBUFFER_SIZE
 from turtle import back
 
 
-class Users:
-    def __init__(self, first_name, last_name, user_name, password, password_confirm, email,  state, county, profile_pic, user_profile, users_database):
+class Users():
+    def __init__(self, first_name = '', last_name = '', user_name = '', password = '', password_confirm = '', email = '',  state = '', county = '', profile_pic = '', user_profile = '', users_database = ''):
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
@@ -78,7 +78,7 @@ class Users:
 
 
 class Lists(Users):
-    def __init__(self, life_time, annual, backyard, bird, year, count_lifetime, count_annual, count_backyard , list_lifetime, list_annual, list_backyard):
+    def __init__(self, life_time ='', annual='', backyard='', bird='', year='', count_lifetime='', count_annual='', count_backyard ='', count_backyard_list=''):
         self.life_time = life_time
         self.annual = annual
         self.backyard = backyard
@@ -87,37 +87,9 @@ class Lists(Users):
         self.count_lifetime = count_lifetime 
         self.count_annual = count_annual
         self.count_backyard = count_backyard
-        self.list_lifetime = list_lifetime
-        self.list_annual = list_annual
-        self.list_backyard = list_backyard
         self.count_backyard_list = count_backyard_list
 
-    def list_choice(self):
-        self.count_lifetime = 0
-        self.count_annual = 0
-        self.count_backyard = 0
-        self.list_lifetime = [] 
-        self.list_annual = [] 
-        self.list_backyard = [] 
-    
-        while True:
-            print("List choices: ")
-            print("\t [1] My backyard list")
-            print("\t [2] Annual list")
-            print("\t [3] Lifetime list")
-            print("\t [4] Finished adding birds for now")
-             # might want to add one-time "outing" dictionary
-            # keys would include: name for outing (invite), location, date, list of birds
-            choice_of_list = input(f"Which of these lists would you like to do [use number]?  ")
 
-            if choice_of_list == '1':
-                self.list_backyard
-            elif choice_of_list == '2':
-                self.list_annual
-            elif choice_of_list == '3':
-                self.list_lifetime
-            elif choice_of_list == '4':
-                break
 
 
 #????? can you help me test this one????#
@@ -133,11 +105,16 @@ class Lists(Users):
             print("\t [4] Finished adding to backyard list for now")
                 # might want to add functionality of this function auto-checking if backyard bird entry is already in annual or lifetime list, and adding if not.
             choice_of_backyard_list = input(f"Use number above to indicate what you'd like to do?  ")
+##--<>start here
+##-****line 116 is returning an object rather than the name of the bird****
+##--*****might be easier to look for a fix inside the test version since it is more contained.
+##--****easy work around would be to change that print statement in 114 to something generic, that doesn't include name of bird
+
 
             if choice_of_backyard_list == '1':
-                self.bird_backyard = input("\nWhich bird would you like to add to your backyard list?  ")
-                print(f"{self.bird_backyard.title} has been added to your list!")
-                self.list_backyard_list.append(self.bird_backyard)
+                bird_backyard = input("\nWhich bird would you like to add to your backyard list?  ").title()
+                print(f"{bird_backyard} has been added to your list!")
+                self.list_backyard_list.append(bird_backyard)
                 self.count_backyard_list = self.count_backyard_list + 1
                 continue
             elif choice_of_backyard_list == '2':    
@@ -154,6 +131,8 @@ class Lists(Users):
             elif choice_of_backyard_list != '1' or '2' or '3' or '4':
                 print("n\Try again, you have not yet entered a valid number.")
                 continue
+
+
 
 #????? How can I reset annual list automatically on December 31 and archive each year's list?
 
@@ -228,19 +207,44 @@ class Lists(Users):
             elif choice_of_lifetime_list != '1' or '2' or '3' or '4':
                 print("n\Try again, you have not yet entered a valid number.")
                 continue
+
+    def list_choice(self):
+        self.count_lifetime = 0
+        self.count_annual = 0
+        self.count_backyard = 0
+    
+        while True:
+            print("List choices: ")
+            print("\t [1] My backyard list")
+            print("\t [2] Annual list")
+            print("\t [3] Lifetime list")
+            print("\t [4] Finished adding birds for now")
+             # might want to add one-time "outing" dictionary
+            # keys would include: name for outing (invite), location, date, list of birds
+            choice_of_list = input(f"Which of these lists would you like to do [use number]?  ")
+
+            if choice_of_list == '1':
+                self.list_backyard()
+            elif choice_of_list == '2':
+                self.list_annual()
+            elif choice_of_list == '3':
+                self.list_lifetime()
+            elif choice_of_list == '4':
+                break
     
 
 
 class Run():
     """Runs program"""
 
-    def __init__(self):
+    def __init__(self, user):
+        self.user = user
     
 #??? do I have to define variables after init statement above?
 
 #???Why does below "def" indicate an indention problem?
     
-    def run_program(self):
+    def run_program(self, the_list_class):
 
         while True:
             print(f"\nWhat would you like to do ?")
@@ -254,21 +258,23 @@ class Run():
             if choice == '1':
                 self.sign_up()
             elif choice == '2':
-                self.list_choice() 
+                the_list_class.list_choice() 
             
             elif choice != '1' or choice != '2' or choice != '3' or choice != '4' or choice != '5' or choice != '6':
                 print("\nYou have not entered a valid number. \nPlease try again.")
                 continue
             else:
                 continue
-user_1 = Users()
+user_1 = Users()  
 
 #???? why does Run say undefined?
 run = Run(user_1)
 
 #????? Are there ways to run individual functions without running entire program (to check for errors)
 
-run.run_program()
+myList = Lists(user_1)
+
+run.run_program(myList)
 
 
 # Can I ingegrate below into one of the classes above?
